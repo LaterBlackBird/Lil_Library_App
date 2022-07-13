7282610eb5806cbcff55fe2641dffa3a2c828609
Seth Holland <sholland@nexient.com>
2022-07-13 15:00:19 -0500

    refactored code, reduced number of db reads on initialization

--- screens/Home.js
+++ screens/Home.js
@@ -10,15 +10,85 @@ import * as geofire from 'geofire-common';
 
 const Home = () => {
 
-  const [location, setLocation] = useState(null);
-  const [markers, setMarkers] = useState([]);
+  const [mapCenter, setMapCenter] = useState(null);
+  const [librariesArray, setLibrariesArray] = useState([]);
   const [searchCriteria, setSearchCriteria] = useState('');
-  const map = useRef();
+  const mapComponentRef = useRef();
 
   useEffect(() => {
-    setInitialLocation();
-    retreiveNearbyLibraries();
+    getInitialLocation();
   }, []);
+
+  const getInitialLocation = async () => {
+    let { status } = await Location.requestForegroundPermissionsAsync();
+    if (status !== 'granted') {
+      setErrorMsg('Permission to access location was denied');
+      return;
+    }
+
+    let location = await Location.getCurrentPositionAsync({});
+    setMapCenter({ latitude: location.coords.latitude, longitude: location.coords.longitude, latitudeDelta: 0.02, longitudeDelta: 0.05 });
+  };
+
+
+
+  useEffect(() => {
+    if (mapCenter !== null) retreiveNearbyLibraries();
+  }, []);
+
+
+  // Find libraries within 10km
+  const retreiveNearbyLibraries = async () => {
+    setLibrariesArray([]);
+
+    const searchCenter = [mapCenter.latitude, mapCenter.longitude];
+    const searchRadius = 10000; //meters
+
+    // Each item in 'bounds' represents a startAt/endAt pair. We have to issue
+    // a separate query for each pair. There can be up to 9 pairs of bounds
+    // depending on overlap, but in most cases there are 4.
+    const bounds = geofire.geohashQueryBounds(searchCenter, searchRadius);
+    const db = collection(fireDB, 'libraries');
+
+    for (const b of bounds) {
+      const q = query(db, orderBy('geohash'), startAt(b[0]), endAt(b[1]))
+      const querySnapshot = await getDocs(q);
+      querySnapshot.forEach(doc => {
+        setLibrariesArray(prevState => [...prevState, { name: doc.data().name, latlng: { latitude: doc.data().location.latitude, longitude: doc.data().location.longitude } }]);
+      })
+    }
+    return 'ok';
+  };
+
+
+
+  const updateMapFromSearch = async () => {
+    const res = await fetch(`https://maps.googleapis.com/maps/api/geocode/json?address=${searchCriteria}&key=${GOOGLE_MAP_API}`)
+    const data = await res.json();
+    setMapCenter({ latitude: data.results[0].geometry.location.lat, longitude: data.results[0].geometry.location.lng, latitudeDelta: 0.02, longitudeDelta: 0.05 });
+  };
+  
+  const updateMapFromMove = async (region) => {
+    if (mapCenter.latitude !== region.latitude && mapCenter.longitude !== region.longitude) {
+      setMapCenter(region);
+      await retreiveNearbyLibraries();
+      return 'ok';
+    } else return 'no update required';
+  };
+
+
+  //Keep this code to use when adding new or editing libraries
+  const returnGeoHashes = async () => {
+    const querySnapshot = await getDocs(collection(fireDB, "libraries"));
+    querySnapshot.forEach((doc) => {
+      const info = doc.data();
+      const lat = info.location.latitude
+      const lng = info.location.longitude
+      const hash = geofire.geohashForLocation([lat, lng]);
+      setLibrariesArray(prevState => [...prevState, { name: info.name, latlng: { latitude: info.location.latitude, longitude: info.location.longitude } }]);
+    });
+  };
+
 
   const handleSignout = () => {
     signOut(fireAuth).then(() => {
@@ -28,68 +98,22 @@ const Home = () => {
     });
   };
 
-  const setInitialLocation = async () => {
-    let { status } = await Location.requestForegroundPermissionsAsync();
-    if (status !== 'granted') {
-      setErrorMsg('Permission to access location was denied');
-      return;
-    }
 
-    let location = await Location.getCurrentPositionAsync({});
-    setLocation({ latitude: location.coords.latitude, longitude: location.coords.longitude, latitudeDelta: 0.02, longitudeDelta: 0.05 });
-  }
-
-  const retreiveNearbyLibraries = async () => {
-    setMarkers([]);
-
-    // Find cities within 50km
-    const center = [location.latitude, location.longitude];
-    const radiusInM = 10 * 1000;
-
-    // Each item in 'bounds' represents a startAt/endAt pair. We have to issue
-    // a separate query for each pair. There can be up to 9 pairs of bounds
-    // depending on overlap, but in most cases there are 4.
-    const bounds = geofire.geohashQueryBounds(center, radiusInM);
-    const dbRef = collection(fireDB, 'libraries');
-
-    for (const b of bounds) {
-      const q = query(dbRef, orderBy('geohash'), startAt(b[0]), endAt(b[1]))
-      const querySnapshot = await getDocs(q);
-      querySnapshot.forEach(doc => {
-        setMarkers(prevState => [...prevState, { name: doc.data().name, latlng: { latitude: doc.data().location.latitude, longitude: doc.data().location.longitude } }]);
-      })
-    }
-  };
-
-
-  const newSearch = (searchText) => {
-    setSearchCriteria(searchText);
-    fetchGeoLocation();
-  }
-
-  const fetchGeoLocation = async () => {
-    const res = await fetch(`https://maps.googleapis.com/maps/api/geocode/json?address=${searchCriteria}&key=${GOOGLE_MAP_API}`)
-    const data = await res.json();
-    setLocation({ latitude: data.results[0].geometry.location.lat, longitude: data.results[0].geometry.location.lng, latitudeDelta: 0.02, longitudeDelta: 0.05 });
-  }
-
-  console.log(markers)
 
   return (
     <View style={styles.container}>
       <MapView
-        ref={map}
+        ref={mapComponentRef}
         provider={PROVIDER_GOOGLE}
         style={styles.map}
-        initialRegion={location}
-        region={location}
+        region={mapCenter}
         rotateEnabled={false}
         zoomControlEnabled={true}
         showsPointsOfInterest={false}
-        onRegionChangeComplete={retreiveNearbyLibraries}
+        onRegionChangeComplete={updateMapFromMove}
       >
-        {markers &&
-          markers.map((marker, index) => (
+        {librariesArray &&
+          librariesArray.map((marker, index) => (
             <Marker
             key={index}
             coordinate={marker.latlng}
@@ -102,7 +126,7 @@ const Home = () => {
         onChangeText={text => setSearchCriteria(text)}
         style={styles.searchBox}
         blurOnSubmit={true}
-        onSubmitEditing={newSearch}
+        onSubmitEditing={updateMapFromSearch}
       />
       <TouchableOpacity
         style={styles.button}

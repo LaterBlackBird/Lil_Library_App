c66061d780991f69403af3211bd28f20a6b4e2ef
Seth Holland <sholland@nexient.com>
2022-07-21 13:36:58 -0500

    link to edit library in place

--- screens/LibraryProfile.js
+++ screens/LibraryProfile.js
@@ -1,16 +1,33 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import React from 'react';
+import { Pressable, StyleSheet, Text, View } from 'react-native';
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
+import { faCircleExclamation } from '@fortawesome/free-solid-svg-icons';
 
 const LibraryProfile = ({ route, navigation }) => {
   const { library } = route.params;
 
 
   return (
-    <View
-      style={styles.container}
-    >
-      <Text style={styles.libraryNameText}>{library.name}</Text>
-      <Text style={styles.libraryEstablishedText}>Established {library.createdAt.toDate().toDateString()}</Text>
+    <View style={styles.container}>
+      <View style={styles.libraryInfo}>
+        <Text
+          style={styles.libraryNameText}
+          numberOfLines={1}
+        >
+          {library.name}
+        </Text>
+
+        <Text style={styles.libraryEstablishedText}>
+          Established {library.createdAt.toDate().toDateString()}
+        </Text>
+
+        <Pressable style={styles.editContainer}>
+          <FontAwesomeIcon icon={faCircleExclamation} style={ styles.exlamationIcon} />
+          <Text style={styles.edit}>
+            Report A Problem
+          </Text>
+        </Pressable>
+      </View>
     </View>
   )
 }
@@ -19,15 +36,32 @@ export default LibraryProfile
 
 const styles = StyleSheet.create({
   container: {
-    justifyContent: 'center',
     alignItems: 'center',
     width: '100%',
     height: '100%',
   },
+  libraryInfo: {
+    top: 50,
+    width: '90%',
+  },
   libraryNameText: {
-    fontSize: 50,
+    fontSize: 30,
+    fontWeight: 'bold',
   },
   libraryEstablishedText: {
-    fontSize: 10,
+    fontSize: 12,
+  },
+  editContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginTop: 10,
+  },
+  edit: {
+    marginLeft: 5,
+    color: 'grey',
+  },
+  exlamationIcon: {
+    padding: 0,
+    color: 'grey',
   },
 })

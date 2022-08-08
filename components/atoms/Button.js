54719837d664384136d6c3f6af608c3e6bdbd102
Seth Holland <sholland@nexient.com>
2022-08-08 15:10:39 -0500

    first passing test

--- components/atoms/Button.js
+++ components/atoms/Button.js
@@ -4,6 +4,7 @@ import React from 'react'
 const Button = ({ onPress, text, buttonStyle}) => {
   return (
     <TouchableOpacity
+      testID='custom-button'
       onPress={onPress}
       style={[
         styles.button,
